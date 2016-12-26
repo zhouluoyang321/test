3bce7dbd412200acba7f06a77864ff1562ca0fd4
zhouluoyang <sx-zhouly@dtdream.com>
2016-12-26 09:26:11 +0800

    代码改动

---
+++ ggs-center/src/main/java/UseUser.java
@@ -0,0 +1,10 @@
+/**
+ * Created by zhouluoyang on 2016/12/23.
+ */
+public class UseUser {
+    public static void main(String[] args) {
+        User user=new User();
+        user.setUsername("ss");
+        System.out.println(user.getUsername());
+    }
+}
---
+++ ggs-center/src/main/java/User.java
@@ -0,0 +1,9 @@
+import lombok.Data;
+
+/**
+ * Created by zhouluoyang on 2016/12/23.
+ */
+@Data
+public class User {
+    private String username;
+}
